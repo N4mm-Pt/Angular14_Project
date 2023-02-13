cb7661ce8e093184af16ab465c6c35ce4da914fe
hiendt <hiendt123@gmail.com>
2023-02-13 16:01:10 +0700

    13/02

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Book } from '../types/Book';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+  @Input() book : Book = {} as Book;
+  @Output() bookEmitter = new EventEmitter<Book>();
+
+
+  addToCart(){
+    this.bookEmitter.emit(this.book);
+  }
+}
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Book } from '../types/Book';
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent {
+  books :Book[] = [
+    {
+      name  : 'Clean Code',
+      author :'Robert C Martin',
+      image :  'https://m.media-amazon.com/images/I/41xShlnTZTL._SX198_BO1,204,203,200_QL40_ML2_.jpg',
+      amount: 700
+    },
+    {
+      name  : ' Pragmatic Programmer',
+      author :'David Thomas',
+      image :  'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+      amount: 800
+    },
+    {
+      name  : ' The Art of Computer Programming',
+      author :'Donald E.Knuth',
+      image :  'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+      amount: 900
+    },
+    {
+      name: 'Introdution To Algorithms',
+      author: 'Thomas H.Cormen',
+      image: 'https://sachtienganhhanoi.com/wp-content/uploads/2022/02/Introduction_to_algorithms-3rd-Edition1.png',
+      amount: 1200
+    }
+  ]
+  cart: Book[] = [];
+   
+  isShowing: boolean = true;
+  
+  ngOnInit(): void{
+
+  }
+  addToCart(book : Book){
+    console.log(book);
+  }
+  
+  
+}
